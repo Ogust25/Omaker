b665cba39548a89948767526b2c8bab3cf9924b5
August Gros <august.gros@gmail.com>
2023-10-13 16:37:41 +0200

    add header and some param

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
 import * as React from 'react';
 import '../styles/global.css';
+import logoOmarker from '../images/blaze.png';
+import gradientBlack from '../images/gradient_black.png';
 
 export default function Index() {
 	return (
-		<>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</>
+		<main className="bg-main">
+			<header className="h-screen flex">
+				<nav className="w-5/6 fixed top-0 left-1/2 transform -translate-x-1/2 pt-5">
+					<ul className="flex flex-row justify-evenly text-2xl font-bebas tracking-widest">
+						<li className="hover:font-bold">PEINTURE</li>
+						<li className="hover:font-bold">TATTOO</li>
+						<li className="hover:font-bold">GRAPHISME</li>
+					</ul>
+				</nav>
+				<img className="m-auto floatAnimation" src={logoOmarker} alt="logo" />
+				<img
+					src={gradientBlack}
+					alt="styling_element"
+					className="absolute bottom-0 right-0"
+				/>
+			</header>
+			<div className="h-screen bg-red-500"></div>
+		</main>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: [
+		'./src/pages/**/*.{js,jsx,ts,tsx}',
+		'./src/components/**/*.{js,jsx,ts,tsx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				main: "url('../images/background.webp')",
+			},
+			fontFamily: {
+				bebas: ['Bebas Neue Pro'],
+			},
+		},
+	},
+	plugins: [],
+};
